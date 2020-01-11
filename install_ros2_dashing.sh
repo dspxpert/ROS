1eae4bd59313d81aea69fb685743717b9f049814
dspxpert <32154318+dspxpert@users.noreply.github.com>
2020-01-11 23:13:19 +0900

    Update install_ros2_dashing.sh

--- install_ros2_dashing.sh
+++ install_ros2_dashing.sh
@@ -12,18 +12,21 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 echo "[Add ROS2 Repository]"
-sudo apt update -y && sudo apt install curl gnupg2 lsb-release -y
+sudo apt update -y
+sudo apt install curl gnupg2 lsb-release -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 
 echo "[Install ROS2 Dashing Desktop]"
 sudo apt update -y
 sudo apt install ros-dashing-desktop -y
-source /opt/ros/dashing/setup.bash
 sudo apt install python3-argcomplete -y
 
-echo "[Set the ROS2 Environment]"
+echo "[Set ROS2 Environment]"
 sh -c "echo \"source /opt/ros/dashing/setup.bash\" >> ~/.bashrc"
 
 echo "[Install Turtlesim]"
 sudo apt install ros-dashing-turtlesim -y
+
+echo "[Execute ROS2 Enviironment]"
+source /opt/ros/dashing/setup.bash

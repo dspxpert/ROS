985b7172c0604229ed1057729858cac74e4f8dfc
dspxpert <32154318+dspxpert@users.noreply.github.com>
2022-02-18 13:16:29 +0900

    Update install_ros2_foxy.sh

--- install_ros2_foxy.sh
+++ install_ros2_foxy.sh
@@ -20,7 +20,7 @@ sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `ls
 #echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 echo "[Install ROS2 Foxy Fitzroy Desktop]"
-sudo apt update -y && sudo apt install -y ros-foxy-desktop
+sudo apt update -y && sudo apt install -y ros-foxy-desktop ros-foxy-rmw-fastrtps* ros-foxy-rmw-cyclonedds*
 sudo apt update -y && sudo apt install -y build-essential cmake git libbullet-dev python3-colcon-common-extensions python3-flake8 python3-pip python3-pytest-cov python3-rosdep python3-setuptools python3-vcstool wget
 python3 -m pip install -U argcomplete flake8-blind-except flake8-builtins flake8-class-newline flake8-comprehensions flake8-deprecated flake8-docstrings flake8-import-order flake8-quotes pytest-repeat pytest-rerunfailures pytest
 sudo apt install --no-install-recommends -y libasio-dev libtinyxml2-dev libcunit1-dev
